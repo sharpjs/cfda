50faf9fa995ff9c13d090299526f2c94ac939fa2
Jeff Sharp <jeff.sharp@git>
2018-01-02 11:28:51 -0600

    Flesh out some ColdFire AST.

--- src/cf/ast.rs
+++ src/cf/ast.rs
@@ -14,11 +14,63 @@
 // You should have received a copy of the GNU General Public License
 // along with cfda.  If not, see <http://www.gnu.org/licenses/>.
 
+use ast::{Expr, Slot};
+use super::reg::{AddrReg, DataReg, PcReg};
+
 /// A ColdFire-specific argument to an operation.
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub enum CfArg {
-    Bc,     // TODO
     Mode,   // TODO
-    Pair,   // TODO
+
+    // Data register pair.
+    DataRegPair(DataRegPair),
+
+    // Cache specifier.
+    Cache(Cache),
+}
+
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+pub enum Loc {
+    DataReg         (Slot<DataReg>),
+    AddrReg         (Slot<AddrReg>),
+    AddrRegInd      (Slot<AddrReg>),
+    AddrRegIndInc   (Slot<AddrReg>),
+    AddrRegIndDec   (Slot<AddrReg>),
+    AddrRegDisp     (Slot<AddrReg>, Slot<Expr>),
+    AddrRegIdx      (Slot<AddrReg>, Slot<Expr>, Slot<Index>),
+    PcDisp          (Slot<PcReg>, Slot<Expr>),
+    PcIdx           (Slot<PcReg>, Slot<Expr>, Slot<Index>),
+    AbsShort        (Slot<Expr>),
+    AbsLong         (Slot<Expr>),
+    Imm             (Slot<Expr>),
+}
+
+/// ColdFire data register pair.
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+pub struct Index {
+    reg:   Slot<IndexReg>,
+    scale: Slot<Expr>,
+}
+
+/// ColdFire data register pair.
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum IndexReg {
+    Data(DataReg),
+    Addr(AddrReg),
+}
+
+/// ColdFire data register pair.
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub struct DataRegPair (
+    Slot<DataReg>,
+    Slot<DataReg>,
+);
+
+/// ColdFire cache selectors.
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum Cache {
+    // Inst,    // ic
+    // Data,    // dc
+    Both,       // bc
 }
 
