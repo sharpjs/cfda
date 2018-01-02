36021d4a30ba862ab5d43531d432dc14dba72892
Jeff Sharp <jeff.sharp@git>
2018-01-02 08:32:33 -0600

    Add type representing an identifier and/or its resolved value.

--- src/ast.rs
+++ src/ast.rs
@@ -28,6 +28,50 @@ pub struct Stmt {
 
 //  /// Arguments to the operation.
 //  pub args: Vec<Arg>,
+
+/// A slot containing a value and/or an identifier that resolves to that value.
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum Slot<V> {
+    /// Unresolved identifier
+    Ident(Ident),
+
+    /// Identifier resolved to a value.
+    Resolved(Ident, V),
+
+    /// Value resolved without an identifier.
+    Value(V),
+}
+
+impl<V> Slot<V> {
+    /// Returns the identifier reference, if any, in the slot.
+    #[inline]
+    pub fn ident(&self) -> Option<Ident> {
+        match *self {
+            Slot::Ident    (i   ) => Some(i),
+            Slot::Resolved (i, _) => Some(i),
+            Slot::Value    (   _) => None,
+        }
+    }
+
+    /// Returns a reference to the resolved value, if any, in the slot.
+    #[inline]
+    pub fn value(&self) -> Option<&V> {
+        match *self {
+            Slot::Ident    (_       ) => None,
+            Slot::Resolved (_, ref v) => Some(v),
+            Slot::Value    (   ref v) => Some(v),
+        }
+    }
+
+    /// Returns a copy of the resolved value, if any, in the slot.
+    #[inline]
+    pub fn value_copy(&self) -> Option<V> where V: Copy {
+        match *self {
+            Slot::Ident    (_   ) => None,
+            Slot::Resolved (_, v) => Some(v),
+            Slot::Value    (   v) => Some(v),
+        }
+    }
 }
 
 /// An assembly operation.
