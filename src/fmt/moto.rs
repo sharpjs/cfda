5d7225db85798ef44e8d52c3e778b414c4b31ab3
Jeff Sharp <jeff.sharp@git>
2018-02-15 06:43:37 -0800

    String formatting. Likely to get moved somewhere else.

--- src/fmt/moto.rs
+++ src/fmt/moto.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with cfda.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::fmt::{Display, Error, Formatter};
+use std::fmt::{Display, Error, Formatter, Write};
 use ast::*;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -42,7 +42,36 @@ impl<'a, 'b: 'a> MotoFormatter<'a, 'b> {
 
 impl<'a, 'b: 'a> Visitor<(), Error> for MotoFormatter<'a, 'b> {
     fn visit_str(&mut self, node: &str) -> Result<(), Error> {
-        self.f.write_str(node)
+        self.f.write_char('"')?;
+
+        for c in node.chars() {
+            fmt_char(c, self.f)?;
+        }
+
+        self.f.write_char('"')
     }
+}
+
+fn fmt_char(c: char, f: &mut Formatter) -> Result<(), Error> {
+    Ok(match c {
+        '\x08'          => f.write_str("\\b")?,
+        '\x09'          => f.write_str("\\t")?,
+        '\x0A'          => f.write_str("\\n")?,
+        '\x0C'          => f.write_str("\\f")?,
+        '\x0D'          => f.write_str("\\r")?,
+        '\"'            => f.write_str("\\\"")?,
+        '\\'            => f.write_str("\\\\")?,
+        '\x20'...'\x7E' => f.write_char(c)?,
+        _               => fmt_esc_utf8(c, f)?
+    })
+}
+
+fn fmt_esc_utf8(c: char, f: &mut Formatter) -> Result<(), Error> {
+    let mut buf = [0u8; 4];
+    let bytes = c.encode_utf8(&mut buf).as_bytes();
+    for &b in bytes {
+        write!(f, "\\x{:02X}", b)?;
+    }
+    Ok(())
 }
 
