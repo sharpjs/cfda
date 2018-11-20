034bf522e7342aca97697de0c18d545eb2209c41
Jeff Sharp <jeff.sharp@git>
2018-11-20 07:12:04 -0600

    Break read_to_buf out into its own file.

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -14,42 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with cfda.  If not, see <http://www.gnu.org/licenses/>.
 
+mod read_to_buf;
 mod rewind_read;
 
-use std::io::{Read, Result};
-use std::io::ErrorKind::*;
-
+pub use self::read_to_buf::*;
 pub use self::rewind_read::*;
-
-/// Extends `std::io::Read` with the `read_to_buf` method.
-pub trait ReadToBuf: Read {
-    /// Read enough bytes to fill `buf`, returning how many bytes were read.
-    ///
-    /// This function is equivalent to `std::io::Read::read_exact()` except for
-    /// its behavior at end of file.  If this function encounters EOF before
-    /// completely filling `buf`, it returns the number of bytes read, which
-    /// will be less than the requested amount.
-    ///
-    fn read_to_buf(&mut self, mut buf: &mut [u8]) -> Result<usize> {
-        let mut total = 0;
-
-        while !buf.is_empty() {
-            let n = match self.read(buf) {
-                Ok(0) => break,
-                Ok(n) => n,
-                Err(ref e) if e.kind() == Interrupted => continue,
-                Err(e) => return Err(e),
-            };
-
-            total += n;
-
-            let tmp = buf;
-            buf = &mut tmp[n..];
-        }
-
-        Ok(total)
-    }
-}
-
-impl<R: Read> ReadToBuf for R { }
 
---
+++ src/io/read_to_buf.rs
@@ -0,0 +1,51 @@
+// This file is part of cfda, a fun little disassembler project.
+// Copyright (C) 2018 Jeffrey Sharp
+//
+// cfda is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// cfda is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+// the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with cfda.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::io::{Read, Result};
+use std::io::ErrorKind::*;
+
+/// Extends `std::io::Read` with the `read_to_buf` method.
+pub trait ReadToBuf: Read {
+    /// Read enough bytes to fill `buf`, returning how many bytes were read.
+    ///
+    /// This function is equivalent to `std::io::Read::read_exact()` except for
+    /// its behavior at end of file.  If this function encounters EOF before
+    /// completely filling `buf`, it returns the number of bytes read, which
+    /// will be less than the requested amount.
+    ///
+    fn read_to_buf(&mut self, mut buf: &mut [u8]) -> Result<usize> {
+        let mut total = 0;
+
+        while !buf.is_empty() {
+            let n = match self.read(buf) {
+                Ok(0) => break,
+                Ok(n) => n,
+                Err(ref e) if e.kind() == Interrupted => continue,
+                Err(e) => return Err(e),
+            };
+
+            total += n;
+
+            let tmp = buf;
+            buf = &mut tmp[n..];
+        }
+
+        Ok(total)
+    }
+}
+
+impl<R: Read> ReadToBuf for R { }
+
