54b99b2c80c9d384159fe19fd56f4a177d115a6d
Jeff Sharp <jeff.sharp@git>
2018-12-01 07:44:45 -0600

    Add trait to set bit fields and tests for both get and set.

--- src/util/field.rs
+++ src/util/field.rs
@@ -14,13 +14,19 @@
 // You should have received a copy of the GNU General Public License
 // along with cfda.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::ops::{Shr, BitAnd};
+use std::ops::{Shl, Shr, BitAnd, BitOr};
 use crate::util::Cast;
 
-/// Access to embedded bit fields.
+/// Trait for reading bit fields within words.
 pub trait Field<P, M> {
     /// Gets the value of the bit field at position `pos`, masked with `mask`.
     fn field(self, pos: P, mask: M) -> M;
+}
+
+/// Trait for writing bit fields within words.
+pub trait SetField<P, M> {
+    /// Sets the value of the bit field at position `pos`, masked with `mask`.
+    fn set_field(self, pos: P, mask: M, value: M) -> Self;
 }
 
 impl<T, P, M> Field<P, M> for T
@@ -31,6 +37,43 @@ where
     #[inline(always)]
     fn field(self, pos: P, mask: M) -> M {
         (self >> pos).cast() & mask
+    }
+}
+
+impl<T, P, M> SetField<P, M> for T
+where
+    T: Shl<P, Output=T> + BitOr<Output=T>,
+    M: BitAnd<Output=M> + Cast<T>
+{
+    #[inline(always)]
+    fn set_field(self, pos: P, mask: M, value: M) -> Self {
+        (value & mask).cast() << pos | self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn field() {
+        //            FEDCBA(987)6543210
+        let word = 0b_111111_101_1111111_u16;
+        let mask =        0b_111_u8;
+        let val  =        0b_101_u8;
+
+        assert_eq!( word.field(7, mask), val );
+    }
+
+    #[test]
+    pub fn set_field() {
+        //            FEDCBA(987)6543210
+        let word = 0b_000000_000_0000000_u16;
+        let mask =        0b_111_u8;
+        let val  =        0b_101_u8;
+        let ret  = 0b_000000_101_0000000_u16;
+
+        assert_eq!( word.set_field(7, mask, val), ret );
     }
 }
 
