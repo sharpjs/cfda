fd2c0756ca832262ccbcdd39d8c6a221b2970bb9
Jeff Sharp <jeff.sharp@git>
2018-12-23 08:11:55 -0600

    Re-add DecodeIndex, improved and simplified.

--- src/decode.rs
+++ src/decode.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with cfda.  If not, see <http://www.gnu.org/licenses/>.
 
+use crate::num::Field;
+
 /// Trait for decoding machine code.
 ///
 /// Type `U` is the unit of storage of machine code.  This type typically is
@@ -34,3 +36,100 @@ pub trait Decode<U, C=()> {
     /// was not successful, this method returns `None`.
     fn decode<'a>(&self, buf: &'a [U], ctx: &C) -> Option<(Self::Output, &'a [U])>;
 }
+
+/// Trait to obtain an opword from a decoding context.
+pub trait Opword {
+    /// The opword type.
+    type Opword;
+
+    /// Returns the opword.
+    fn opword(&self) -> Self::Opword;
+}
+
+/// A data structure for efficient decoding of instructions.
+///
+/// A `DecodeIndex` is a recursive, tree-shaped structure.  An index consists
+/// of a single node, which can be leaf or non-leaf.  A leaf node either is
+/// empty or references a single item.  A non-leaf node references a set of
+/// subnodes, each of which is an independent index.  A non-leaf node also
+/// indicates how the appropriate subnode is chosen during an index lookup.
+#[derive(Debug)]
+pub enum DecodeIndex<T: 'static> {
+
+    // === LEAF ===
+
+    /// An empty leaf node.
+    Empty,
+
+    /// A leaf node with one item.
+    Leaf(&'static T),
+
+    // === SCAN ===
+
+    /// Two subnodes, selected by sequential scan.
+    Scan2(&'static [DecodeIndex<T>; 2]),
+
+    /// Three subnodes, selected by sequential scan.
+    Scan3(&'static [DecodeIndex<T>; 3]),
+
+    /// Four subnodes, selected by sequential scan.
+    Scan4(&'static [DecodeIndex<T>; 4]),
+
+    // === TRIE ===
+
+    /// 2 subnodes, selected by the 1-bit field at the given position.
+    Trie2(&'static [DecodeIndex<T>; 2], u8),
+
+    /// 4 subnodes, selected by the 2-bit field whose least significant bit is
+    /// at the given position.
+    Trie4(&'static [DecodeIndex<T>; 4], u8),
+
+    /// 8 subnodes, selected by the 3-bit field whose least significant bit is
+    /// at the given position.
+    Trie8(&'static [DecodeIndex<T>; 8], u8),
+
+    /// 16 subnodes, selected by the 4-bit field whose least significant bit is
+    /// at the given position.
+    Trie16(&'static [DecodeIndex<T>; 16], u8),
+}
+
+impl<T, U, C> Decode<U, C> for DecodeIndex<T>
+where
+    T: Decode<U, C>,
+    C: Opword,
+    C::Opword: Field<u8, u8>
+{
+    type Output = T::Output;
+
+    fn decode<'a>(&self, buf: &'a [U], ctx: &C) -> Option<(T::Output, &'a [U])> {
+        use self::DecodeIndex::*;
+
+        enum Plan<T: 'static> {
+            Scan(&'static [DecodeIndex<T>]),
+            Seek(&'static [DecodeIndex<T>], u8, u8),
+        }
+
+        let plan = match *self {
+            Empty               => return None,
+            Leaf   (item)       => return item.decode(buf, ctx),
+            Scan2  (nodes)      => Plan::Scan(nodes),
+            Scan3  (nodes)      => Plan::Scan(nodes),
+            Scan4  (nodes)      => Plan::Scan(nodes),
+            Trie2  (nodes, pos) => Plan::Seek(nodes, pos, 0b0001),
+            Trie4  (nodes, pos) => Plan::Seek(nodes, pos, 0b0011),
+            Trie8  (nodes, pos) => Plan::Seek(nodes, pos, 0b0111),
+            Trie16 (nodes, pos) => Plan::Seek(nodes, pos, 0b1111),
+        };
+
+        match plan {
+            Plan::Scan(nodes) => {
+                nodes.iter().find_map(|n| n.decode(buf, ctx))
+            },
+            Plan::Seek(nodes, pos, mask) => {
+                let val = ctx.opword().field(pos, mask);
+                nodes[val as usize].decode(buf, ctx)
+            },
+        }
+    }
+}
+
